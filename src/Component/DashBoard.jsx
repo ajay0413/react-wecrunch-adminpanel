c99f8bb5c290301c3dedb949d1f009ab496fcc02
ajay0413 <ajayr77713@gmail.com>
2024-06-22 16:32:24 +0530

    fixed

--- src/Component/DashBoard.jsx
+++ src/Component/DashBoard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import TableIdCardcomp from './common/TableIdCardcomp'
-import IncomebarGraph from './common/IncomeBarGraph'
+// import IncomebarGraph from './common/IncomeBarGraph'
 
 export default function DashBoard() {
   return (
@@ -9,7 +9,7 @@ export default function DashBoard() {
    <TableIdCardcomp/>  
 
    
-   <IncomebarGraph/> 
+   {/* <IncomebarGraph/>  */}
     
 
     </div>
